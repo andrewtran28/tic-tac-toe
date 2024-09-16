ec3fa472239c483a8d6eed22f98f7822f4d7ebc1
andrewtran28 <tran.andrew@outlook.com>
2024-09-16 15:21:38 -0400

    Round gameplay functioning (need to stylize and add round winning)

--- scripts.js
+++ scripts.js
@@ -1,82 +1,84 @@
-//Player factory, need to update to prompt for name in a form.
-const Player = (name, marker) => {
-    return {name, marker};
-}
-const player1 = Player("Player 1", "X");
-const player2 = Player("Player 2", "O");
+const createGameBoard = (() => {
+    const gameCont = document.querySelector(".game-container");
+    const btn_name = document.querySelector(".nameButton");
+    const namePlayer1 = document.querySelector("#player1");
+    const namePlayer2 = document.querySelector("#player2");
+    const form = document.querySelector(".form");
 
-//Gameboard module which contains all functions for controlling the board
-const Gameboard = (() => {
-    const row = 3;
     let board = new Array(9);
+    let activePlayer = 0; //Player1 starts if true
+    let round =  0;
+    let message = "";
+    // let gameOver = false;
 
-    const getBoard = () => board;
+    // //Player factory, need to update to prompt for name in a form.
+    const Player = (name, score) => {
+        return {name, score};
+    }
 
-    const resetBoard = () => {
-        board = Array.from({length: 9}, () => Array(9).fill(' '));
+    player1 = Player("Player 1", 0);
+    player2 = Player("Player 2", 0);
+
+    const initializeGameBoard = () => {                     //"startGameBoard"
+        activePlayer = 0;
+        marker = "X";
+        message = "It is " + player1.name + "'s turn.";
+        
+        board = new Array(9);
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "-";
+        }
     };
 
-    const addMarker = (marker, space) => {
-        if (board[space] === "-") {
-            board[space] = marker;
-            return true;
-        }
+    const refreshGameBoard = function () {
+        gameCont.innerHTML = "";
+        getGameBoard();
+      };
 
-        return false;
+    const getGameBoard = () => {                            //"showGameBoard"
+        console.log("active player: " + activePlayer);
+        board.forEach((item, index) => {                  //need to reference player.marker here
+            const cell = document.createElement("div");
+            cell.classList.add('cell');
+            cell.textContent = item;
+            cell.id = index;
+            cell.addEventListener("click", addMarker);
+            gameCont.appendChild(cell);
+        });
     }
 
-    return {getBoard, resetBoard, addMarker};
-})();
-
-//DOM controller module which contains all functions that manipulate the DOM
-const DOMController = ((gameboard) => {
-    const boardElement = document.getElementById("game-board");
-    const message = document.getElementById("message");
-
-    const initializeBoard = (game) => {
-        const board = gameboard.getBoard();
-
-        boardElement.innerhtml = "";
-
-        for (let i = 0; i < board.length; i++) {
-                const cell = document.createElement('div');
-                cell.classList.add('cell');
-                // cell.dataset.row = i;
-                cell.addEventListener('click', () => game.playTurn(i));
-                cell.textContent = i;
-                boardElement.appendChild(cell);
-        }
+    const addMarker = (e) => {                               //"scrnUpdate"
+            let id = e.target.id;
+            if (board[id] == "-") {
+                board[id] = marker;
+                refreshGameBoard();
+                checkWin();
+            }
     }
-
-    const updateBoard = (space, marker) => {
-        const cell = boardElement.querySelector(".cell");
-        cell.textContent = marker;
-    }
-
-    const setMessage = (message) => {
-        message.textContent = message;
-    }
-
-    const resetButton = (game) => {
-        const btn_reset = document.querySelector("#reset-button");
-
-        btn_reset.addEventListener("click", () => game.resetGame());
-    }
-
-    return {initializeBoard, updateBoard, setMessage, resetButton};
-
-})(Gameboard);
-
-//Game controller module which contains all functions for controlling the gameflow
-const GameController = ((player1, player2, gameboard, domController) => {
-    let activePlayer = player1; //Initialize activePlayer as the first player.
-    let gameOver = false;
 
     const switchPlayer = () => {
-        activePlayer === player1 ? activePlayer = player2 : activePlayer = player1;
+        if (activePlayer == 0) {
+            activePlayer = 1
+            marker = "O";
+        }
+
+        else if (activePlayer == 1) {
+            activePlayer = 0
+            marker = "X"
+        }
     }
 
-    const checkWin = (board, marker) => {
+    const winRound = () => {
+        if (activePlayer == 0) {
+            player1.score++;
+        }
+
+        else if (activePlayer == 1) {
+            player2.score++;
+        }
+    }
+
+    const checkWin = () => {
         if  ((board[0] !== "-" && board[0] == board[1] && board[1] == board[2] ) ||
         (board[3] !== "-" && board[3] == board[4] && board[4] == board[5] ) ||
         (board[6] !== "-" && board[6] == board[7] && board[7] == board[8] ) ||
@@ -85,62 +87,175 @@ const GameController = ((player1, player2, gameboard, domController) => {
         (board[2] !== "-" && board[2] == board[5] && board[5] == board[8] ) ||
         (board[0] !== "-" && board[0] == board[4] && board[4] == board[8] ) ||
         (board[2] !== "-" && board[2] == board[4] && board[4] == board[6] )) {
-            return true;
+            winRound();
+            console.log("Games over by win");
+            initializeGameBoard();
+            round++;
         }
 
         else if (!board.includes("-")) {
-            return true;
+            //Tie: send message and restart board for new round
+            console.log("Games over by tie");
+            initializeGameBoard();
+            round++;
         }
 
         else {
-            return false;
-        }
-    };
-
-    const checkTie = (board, marker) => {
-        if (!board.includes("-")) {
-            return true;
-        }
-    };
-
-    const playTurn = (space) => {
-        if (gameOver) return;
-
-        if (gameboard.addMarker(activePlayer.marker, space)) {
-            const board = gameboard.getBoard();
-
-            domController.updateBoard(activePlayer.marker, space)
-
-            if (checkWin(board, activePlayer.marker)) {
-                domController.setMessage(`${activePlayer.name} wins!`)
-                gameOver = true;
-                return;
-            }
-
-            if (checkTie(board, activePlayer.marker)) {
-                domController.setMessage("It's a tie!")
-                gameOver = true;
-                return;
-
-            }
-
+            //No win or tie yet, switch players and continue.
             switchPlayer();
-            domController.setMessage(`It's ${currentPlayer.name}'s turn.`);
         }
-    }
+    };
 
-    const resetGame = () => {
-        gameboard.resetBoard();
-        domController.initializeBoard(GameController);
-        domController.setMessage(`It's ${player1.name}'s turn`);
-        currentPlayer = player1;
-        gameOver = false;
-    }
+    return {initializeGameBoard, getGameBoard}
 
-    return {playTurn, resetGame};
+})();
 
-})(player1, player2, Gameboard, DOMController);
+createGameBoard.initializeGameBoard();
+createGameBoard.getGameBoard();
 
-DOMController.initializeBoard(GameController);
-DOMController.setMessage(`It's ${player1.name} turn`);
-DOMController.resetButton(GameController);
+// //Player factory, need to update to prompt for name in a form.
+// const Player = (name, marker) => {
+//     return {name, marker};
+// }
+// const player1 = Player("Player 1", "X");
+// const player2 = Player("Player 2", "O");
+
+// //Gameboard module which contains all functions for controlling the board
+// const Gameboard = (() => {
+//     const row = 3;
+//     let board = new Array(9);
+
+//     const getBoard = () => board;
+
+//     const resetBoard = () => {
+//         board = Array.from({length: 9}, () => Array(9).fill(' '));
+//     };
+
+//     const addMarker = (marker, space) => {
+//         if (board[space] === "-") {
+//             board[space] = marker;
+//             return true;
+//         }
+
+//         return false;
+//     }
+
+//     return {getBoard, resetBoard, addMarker};
+// })();
+
+// //DOM controller module which contains all functions that manipulate the DOM
+// const DOMController = ((gameboard) => {
+//     const boardElement = document.getElementById("game-board");
+//     const message = document.getElementById("message");
+
+//     const initializeBoard = (game) => {
+//         const board = gameboard.getBoard();
+
+//         boardElement.innerhtml = "";
+
+//         for (let i = 0; i < board.length; i++) {
+//                 const cell = document.createElement('div');
+//                 cell.classList.add('cell');
+//                 // cell.dataset.row = i;
+//                 cell.addEventListener('click', () => game.playTurn(i));
+//                 cell.textContent = i;
+//                 boardElement.appendChild(cell);
+//         }
+//     }
+
+//     const updateBoard = (space, marker) => {
+//         const cell = boardElement.querySelector(".cell");
+//         cell.textContent = marker;
+//     }
+
+//     const setMessage = (message) => {
+//         message.textContent = message;
+//     }
+
+//     const resetButton = (game) => {
+//         const btn_reset = document.querySelector("#reset-button");
+
+//         btn_reset.addEventListener("click", () => game.resetGame());
+//     }
+
+//     return {initializeBoard, updateBoard, setMessage, resetButton};
+
+// })(Gameboard);
+
+// //Game controller module which contains all functions for controlling the gameflow
+// const GameController = ((player1, player2, gameboard, domController) => {
+//     let activePlayer = player1; //Initialize activePlayer as the first player.
+//     let gameOver = false;
+
+//     const switchPlayer = () => {
+//         activePlayer === player1 ? activePlayer = player2 : activePlayer = player1;
+//     }
+
+//     const checkWin = (board, marker) => {
+//         if  ((board[0] !== "-" && board[0] == board[1] && board[1] == board[2] ) ||
+//         (board[3] !== "-" && board[3] == board[4] && board[4] == board[5] ) ||
+//         (board[6] !== "-" && board[6] == board[7] && board[7] == board[8] ) ||
+//         (board[0] !== "-" && board[0] == board[3] && board[3] == board[6] ) ||
+//         (board[1] !== "-" && board[1] == board[4] && board[4] == board[7] ) ||
+//         (board[2] !== "-" && board[2] == board[5] && board[5] == board[8] ) ||
+//         (board[0] !== "-" && board[0] == board[4] && board[4] == board[8] ) ||
+//         (board[2] !== "-" && board[2] == board[4] && board[4] == board[6] )) {
+//             return true;
+//         }
+
+//         else if (!board.includes("-")) {
+//             return true;
+//         }
+
+//         else {
+//             return false;
+//         }
+//     };
+
+//     const checkTie = (board, marker) => {
+//         if (!board.includes("-")) {
+//             return true;
+//         }
+//     };
+
+//     const playTurn = (space) => {
+//         if (gameOver) return;
+
+//         if (gameboard.addMarker(activePlayer.marker, space)) {
+//             const board = gameboard.getBoard();
+
+//             domController.updateBoard(activePlayer.marker, space)
+
+//             if (checkWin(board, activePlayer.marker)) {
+//                 domController.setMessage(`${activePlayer.name} wins!`)
+//                 gameOver = true;
+//                 return;
+//             }
+
+//             if (checkTie(board, activePlayer.marker)) {
+//                 domController.setMessage("It's a tie!")
+//                 gameOver = true;
+//                 return;
+
+//             }
+
+//             switchPlayer();
+//             domController.setMessage(`It's ${currentPlayer.name}'s turn.`);
+//         }
+//     }
+
+//     const resetGame = () => {
+//         gameboard.resetBoard();
+//         domController.initializeBoard(GameController);
+//         domController.setMessage(`It's ${player1.name}'s turn`);
+//         currentPlayer = player1;
+//         gameOver = false;
+//     }
+
+//     return {playTurn, resetGame};
+
+// })(player1, player2, Gameboard, DOMController);
+
+// DOMController.initializeBoard(GameController);
+// DOMController.setMessage(`It's ${player1.name} turn`);
+// DOMController.resetButton(GameController);
